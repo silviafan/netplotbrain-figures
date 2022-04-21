c666c1e1e88e5e253c734f5c4707e3f9bff732cd
Silvia Fanton <66485342+silviafan@users.noreply.github.com>
2022-04-21 15:04:39 +0200

    pulling from tf

--- components.py
+++ components.py
@@ -1,16 +1,21 @@
 # This notebook goes through some netplotbrain examples to plot each component individually and then together
 
 # Import everything needed
+import numpy as np
 import netplotbrain
 import pandas as pd
 import matplotlib.pyplot as plt
+import templateflow.api as tf
 
 # Example node and edges dataframes
-url = 'https://raw.githubusercontent.com/ThomasYeoLab/CBIG/master/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Centroid_coordinates/Schaefer2018_100Parcels_7Networks_order_FSLMNI152_2mm.Centroid_RAS.csv'
-nodes = pd.read_csv(url,index_col=0, parse_dates=[0])
-nodes.columns = ['name', 'x', 'y', 'z']
-networks = list(map(lambda x: x.split('_')[2], nodes.name.values))
-nodes['community'] = networks
+atlasinfo = tf.get(template='MNI152NLin6Asym',
+       atlas='Schaefer2018',
+       desc='100Parcels7Networks',
+       extension='.tsv')
+atlasinfo = pd.read_csv(atlasinfo, sep='\t')
+# Parse the info in to get network names
+networks = list(map(lambda x: x.split('_')[2], atlasinfo.name.values))
+atlasinfo['yeo7networks'] = networks
 
 edges = pd.read_csv('./edges.tsv', sep='\t', index_col=0)
 
@@ -28,19 +33,24 @@ netplotbrain.plot(template='MNI152NLin6Asym',
 
 # Plot only nodes
 ax = fig.add_subplot(142, projection='3d')
-netplotbrain.plot(nodes=nodes,
-                 template='MNI152NLin6Asym',
-                 templatestyle=None,
-                 view='L',
-                 nodetype='spheres',
-                 nodecolor='Salmon',
-                 arrowaxis=[],
-                 title='',
-                 fig=fig, ax=ax)
-
+netplotbrain.plot(nodes=atlasinfo,
+                  nodeimg={'atlas': 'Schaefer2018',
+                           'desc': '100Parcels7Networks',
+                           'resolution': 1},
+                  template='MNI152NLin6Asym',
+                  templatestyle=None,
+                  templatealpha=0.08,
+                  nodetype='spheres',
+                  view='L',
+                  arrowaxis=[],
+                  title='',
+                  fig=fig, ax=ax)
 # Plot only edges
 ax = fig.add_subplot(143, projection='3d')
-netplotbrain.plot(nodes=nodes,
+netplotbrain.plot(nodes=atlasinfo,
+                 nodeimg={'atlas': 'Schaefer2018',
+                           'desc': '100Parcels7Networks',
+                           'resolution': 1},                  
                  edges=edges,
                  template='MNI152NLin6Asym',
                  templatestyle=None,
@@ -54,11 +64,14 @@ netplotbrain.plot(nodes=nodes,
 
 # Plot template, nodes, and edges together
 ax = fig.add_subplot(144, projection='3d')
-netplotbrain.plot(nodes=nodes,
+netplotbrain.plot(nodes=atlasinfo,
+                 nodeimg={'atlas': 'Schaefer2018',
+                           'desc': '100Parcels7Networks',
+                           'resolution': 1},                                    
                  edges=edges,
                  template='MNI152NLin6Asym',
                  templatestyle='surface',
-                 templatealpha=0.07,
+                 templatealpha=0.08,
                  view='L',
                  nodetype='spheres',
                  nodecolor='Salmon',
