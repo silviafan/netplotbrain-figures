2dfd3fc5fd9cca8cf8694ca4af7e227acd781327
Silvia Fanton <66485342+silviafan@users.noreply.github.com>
2022-08-23 18:30:20 +0200

    views and rotation figs and code

--- views.py
+++ views.py
@@ -4,83 +4,79 @@
 import netplotbrain
 import pandas as pd
 import matplotlib.pyplot as plt
-import templateflow.api as tf
 
-# Example node and edges dataframes
-atlasinfo = tf.get(template='MNI152NLin6Asym',
-       atlas='Schaefer2018',
-       desc='100Parcels7Networks',
-       extension='.tsv')
-atlasinfo = pd.read_csv(atlasinfo, sep='\t')
-# Parse the info in to get network names
-networks = list(map(lambda x: x.split('_')[2], atlasinfo.name.values))
-atlasinfo['yeo7networks'] = networks
+nodes = pd.read_csv('./example_nodes.tsv', sep='\t', index_col=0)
+edges = pd.read_csv('./example_edges.tsv', sep='\t', index_col=0)
 
-edges = pd.read_csv('./edges.tsv', sep='\t', index_col=0)
 
-# Multiple views on multiple rows
-netplotbrain.plot(nodes=atlasinfo,
-                  nodeimg={'atlas': 'Schaefer2018',
-                            'desc': '100Parcels7Networks',
-                            'resolution': 1},
+# 8 views on multiple rows
+netplotbrain.plot(nodes=nodes,
                   edges=edges,
                   edgescale=5,
                   template='MNI152NLin6Asym',
                   templatestyle='glass',
-                  template_glass_maxalpha=0.03,
-                  view=['LSR', 'AIP'],
-                  nodecolorby='yeo7networks',
-                  nodecmap='inferno',
+                  template_glass_maxalpha=0.07,
+                  view='preset-6',
+                  nodecolor='community',
+                  nodecmap='viridis',
                   nodetype='circles',
-                  nodescale=10,
+                  nodescale=12,
                   nodealpha=1,
                   arrowaxis=None,
                   showlegend=False)
 
-plt.savefig('./figures/views.jpg', dpi=300)
+plt.savefig('./figures/preset6.png', dpi=300)
 
-# Rotations AP with 8 frames
-netplotbrain.plot(nodes=atlasinfo,
-                  nodeimg={'atlas': 'Schaefer2018',
-                            'desc': '100Parcels7Networks',
-                            'resolution': 1},
+# 4 views on multiple rows
+netplotbrain.plot(nodes=nodes,
                   edges=edges,
                   edgescale=5,
                   template='MNI152NLin6Asym',
                   templatestyle='glass',
-                  template_glass_maxalpha=0.03,
-                  templatecolor='black',
+                  template_glass_maxalpha=0.07,
+                  view='preset-4',
+                  nodecolor='community',
+                  nodecmap='viridis',
+                  nodetype='circles',
+                  nodescale=12,
+                  nodealpha=1,
+                  arrowaxis=None,
+                  showlegend=False)
+
+plt.savefig('./figures/preset4.png', dpi=300)
+
+# Rotations AP with 4 frames
+netplotbrain.plot(nodes=nodes,
+                  edges=edges,
+                  edgescale=5,
+                  template='MNI152NLin6Asym',
+                  templatestyle='glass',
+                  template_glass_maxalpha=0.07,
                   view=['AP'],
-                  frames=8,
-                  nodecolorby='yeo7networks',
+                  frames=4,
+                  nodecolor='community',
                   nodecmap='cividis',
                   nodetype='circles',
-                  nodescale=10,
+                  nodescale=12,
                   nodealpha=1,
-                  arrowaxis=None,
                   showlegend=False)
 
-plt.savefig('./figures/rotation_8frames.png', dpi=300)
+plt.savefig('./figures/4framesrot.png', dpi=300)
 
 # Rotations AP with 5 frames
-netplotbrain.plot(nodes=atlasinfo,
-                  nodeimg={'atlas': 'Schaefer2018',
-                            'desc': '100Parcels7Networks',
-                            'resolution': 1},
+netplotbrain.plot(nodes=nodes,
                   edges=edges,
                   edgescale=5,
                   template='MNI152NLin6Asym',
                   templatestyle='glass',
-                  template_glass_maxalpha=0.03,
-                  templatecolor='black',
+                  template_glass_maxalpha=0.07,
                   view=['AP'],
                   frames=5,
-                  nodecolorby='yeo7networks',
+                  nodecolor='community',
                   nodecmap='cividis',
                   nodetype='circles',
-                  nodescale=10,
+                  nodescale=12,
                   nodealpha=1,
-                  arrowaxis=None,
                   showlegend=False)
 
-plt.savefig('./figures/rotation_5frames.png', dpi=300)
+plt.savefig('./figures/5framesrot.png', dpi=300)
