c6b41928c009939e7795d359294ed1acbc4c5223
Silvia Fanton <66485342+silviafan@users.noreply.github.com>
2022-09-06 11:23:57 +0200

    different tf templates

--- tf_options.py
+++ tf_options.py
@@ -4,45 +4,40 @@
 import netplotbrain
 import matplotlib.pyplot as plt
 
-# Example node and edges dataframes
-
-fig  = plt.figure()
-
 # Adult brain MNI152NLin6Asym
-ax = fig.add_subplot(141, projection='3d')
 netplotbrain.plot(template='MNI152NLin6Asym',
                   templatestyle='glass',
                   view='L',
                   arrowaxis=None,
-                  subtitles='MNI152NLin6Asym',
-                  fig=fig, ax=ax)
+                  subtitles='MNI152NLin6Asym')
+
+plt.savefig('./figures/adultMNI.png', dpi=300)
 
 # Infant template MNIInfant_cohort-3
 # Setting a larger templatevoxsize will make it slightly quicker
-ax = fig.add_subplot(142, projection='3d')
 netplotbrain.plot(template='MNIInfant_cohort-3',
                   templatestyle='glass',
                   view='L',
                   arrowaxis=None,
-                  subtitles='MNIInfant_cohort-3',
-                  fig=fig, ax=ax)
+                  subtitles='MNIInfant_cohort-3')
+
+plt.savefig('./figures/infantMNIcohort3.png', dpi=300)
 
 # Rat template WHS
-ax = fig.add_subplot(143, projection='3d')
 netplotbrain.plot(template='WHS',
                   templatestyle='glass',
                   view='L',
                   arrowaxis=None,
-                  subtitles='WHS',
-                  fig=fig, ax=ax)
+                  subtitles='WHS')
+
+plt.savefig('./figures/ratWHS.png', dpi=300)
+
 
 # Squirrel monkey VALiDATe29 
-ax = fig.add_subplot(144, projection='3d')
 netplotbrain.plot(template='VALiDATe29',
                   templatestyle='glass',
                   view='L',
                   arrowaxis=None,
-                  subtitles='VALiDATe29',
-                  fig=fig, ax=ax)
+                  subtitles='VALiDATe29')
 
-fig.savefig('./figures/diff_tftemplates.png', dpi=300)
+plt.savefig('./figures/squirrelmonkeyValidate29.png', dpi=300)
