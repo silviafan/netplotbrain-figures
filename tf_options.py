b784dd143acfbdff98d0d547d630bc3514e69850
Silvia Fanton <66485342+silviafan@users.noreply.github.com>
2022-09-06 09:52:44 +0200

    change to template options

--- tf_options.py
+++ tf_options.py
@@ -1,71 +1,48 @@
-# Plotting adult, infant, and brain templates pulled from TemplateFlow https://www.templateflow.org
+# Plotting templates pulled from TemplateFlow https://www.templateflow.org
 
 # Import everything needed
 import netplotbrain
-import pandas as pd
 import matplotlib.pyplot as plt
 
 # Example node and edges dataframes
 
 fig  = plt.figure()
 
-# Adult brain
-ax = fig.add_subplot(131, projection='3d')
-netplotbrain.plot(nodes={'atlas': 'Schaefer2018',
-                         'desc': '100Parcels7Networks',
-                         'resolution': 1},
-                  template='MNI152NLin6Asym',
-                  templatestyle='surface',
-                  view='S',
-                  nodecolor='blue',
-                  nodealpha=1,
-                  nodescale=15,
+# Adult brain MNI152NLin6Asym
+ax = fig.add_subplot(141, projection='3d')
+netplotbrain.plot(template='MNI152NLin6Asym',
+                  templatestyle='glass',
+                  view='L',
                   arrowaxis=None,
-                  showlegend=False,
-                  subtitles='Adult template MNI152NLin6Asym',
+                  subtitles='MNI152NLin6Asym',
                   fig=fig, ax=ax)
 
-# Example node dataframe
-nodes = pd.read_csv('./example_nodes.tsv', sep='\t', index_col=0)
-
-# Rat template
-nodes_whs = nodes.copy()
-nodes_whs['x'] = nodes_whs['x'] / 8
-nodes_whs['y'] = nodes_whs['y'] / 8
-nodes_whs['z'] = nodes_whs['z'] / 8
-
-ax = fig.add_subplot(132, projection='3d')
-netplotbrain.plot(nodes=nodes_whs,
-                  template='WHS',
-                  templatestyle='surface',
-                  templatealpha=0.3,
-                  view='S',
-                  nodescale=15,
-                  nodecolor='blue',
-                  nodealpha=1,
-                  arrowaxis=None,
-                  showlegend=False,
-                  subtitles='Rat template WHS',
-                  fig=fig, ax=ax)
-
-# Infant template
-nodes_inf = nodes.copy()
-nodes_inf['x'] = nodes_inf['x'] / 1.25
-nodes_inf['y'] = nodes_inf['y'] / 1.25
-nodes_inf['z'] = nodes_inf['z'] / 1.25
-
+# Infant template MNIInfant_cohort-3
 # Setting a larger templatevoxsize will make it slightly quicker
-ax = fig.add_subplot(133, projection='3d')
-netplotbrain.plot(nodes=nodes_inf,
-                  template='MNIInfant_cohort-3',
-                  templatestyle='surface',
-                  templatealpha=0.3,
-                  view='S',
-                  nodecolor='blue',
-                  nodescale=15,
-                  showlegend=False,
+ax = fig.add_subplot(142, projection='3d')
+netplotbrain.plot(template='MNIInfant_cohort-3',
+                  templatestyle='glass',
+                  view='L',
                   arrowaxis=None,
-                  subtitles='Infant template MNIInfant',
+                  subtitles='MNIInfant_cohort-3',
                   fig=fig, ax=ax)
 
-fig.savefig('./figures/diff_tftemplates.png', dpi=150)
+# Rat template WHS
+ax = fig.add_subplot(143, projection='3d')
+netplotbrain.plot(template='WHS',
+                  templatestyle='glass',
+                  view='L',
+                  arrowaxis=None,
+                  subtitles='WHS',
+                  fig=fig, ax=ax)
+
+# Squirrel monkey VALiDATe29 
+ax = fig.add_subplot(144, projection='3d')
+netplotbrain.plot(template='VALiDATe29',
+                  templatestyle='glass',
+                  view='L',
+                  arrowaxis=None,
+                  subtitles='VALiDATe29',
+                  fig=fig, ax=ax)
+
+fig.savefig('./figures/diff_tftemplates.png', dpi=300)
