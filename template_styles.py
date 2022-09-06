9a8e05256703f821614e1a77f7d7a7572ba0b931
Silvia Fanton <66485342+silviafan@users.noreply.github.com>
2022-09-06 09:07:40 +0200

    small adjustments

--- template_styles.py
+++ template_styles.py
@@ -6,43 +6,43 @@ import matplotlib.pyplot as plt
 
 fig  = plt.figure()
 
-# Plot surface style 
+# Plot glass style 
 ax = fig.add_subplot(141, projection='3d')
 netplotbrain.plot(template='MNI152NLin6Asym',
+                  templatestyle='glass',
+                  view='S',
+                  arrowaxis=None,
+                  subtitles='glass',
+                  fig=fig, ax=ax)
+
+# Plot surface style 
+ax = fig.add_subplot(142, projection='3d')
+netplotbrain.plot(template='MNI152NLin6Asym',
                   templatestyle='surface',
-                  view='A',
+                  view='S',
                   arrowaxis=None,
                   subtitles='surface',
                   fig=fig, ax=ax)
 
 # Plot filled style 
-ax = fig.add_subplot(142, projection='3d')
+ax = fig.add_subplot(143, projection='3d')
 netplotbrain.plot(template='MNI152NLin6Asym',
                   templatestyle='filled',
-                  view='A',
+                  view='S',
                   arrowaxis=None,
                   subtitles='filled',
                   fig=fig, ax=ax)
 
 # Plot cloudy style 
-ax = fig.add_subplot(143, projection='3d')
+ax = fig.add_subplot(144, projection='3d')
 netplotbrain.plot(template='MNI152NLin6Asym',
                   templatestyle='cloudy',
                   templatevoxelsize=2,
                   templatealpha=0.065,
-                  view='A',
+                  view='S',
+                  templateedgethreshold=0.6,
                   arrowaxis=None,
                   subtitles='cloudy',
-                  fig=fig, ax=ax)
-
-# Plot glass style 
-ax = fig.add_subplot(144, projection='3d')
-netplotbrain.plot(template='MNI152NLin6Asym',
-                  templatestyle='glass',
-                  view='A',
-                  arrowaxis=None,
-                  subtitles='glass',
-                  template_glass_maxalpha=0.035,
                   fig=fig, ax=ax)
 
 fig.savefig('./figures/template_styles.png', dpi=300)
