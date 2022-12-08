134a440a497087ea5dd84dc0476009f30ccb7a0a
Silvia Fanton <66485342+silviafan@users.noreply.github.com>
2022-12-08 10:29:58 +0100

    tf options figures and code

--- tf_options.py
+++ tf_options.py
@@ -6,38 +6,42 @@ import matplotlib.pyplot as plt
 
 # Adult brain MNI152NLin6Asym
 netplotbrain.plot(template='MNI152NLin6Asym',
-                  templatestyle='glass',
+                  template_style='glass',
                   view='L',
                   arrowaxis=None,
-                  subtitles='MNI152NLin6Asym')
+                  title='MNI152NLin6Asym',
+                  subtitles=None)
 
 plt.savefig('./figures/adultMNI.png', dpi=300)
 
 # Infant template MNIInfant_cohort-3
 # Setting a larger templatevoxsize will make it slightly quicker
 netplotbrain.plot(template='MNIInfant_cohort-3',
-                  templatestyle='glass',
+                  template_style='glass',
                   view='L',
                   arrowaxis=None,
-                  subtitles='MNIInfant_cohort-3')
+                  title='MNIInfant_cohort-3',
+                  subtitles=None)
 
 plt.savefig('./figures/infantMNIcohort3.png', dpi=300)
 
 # Rat template WHS
 netplotbrain.plot(template='WHS',
-                  templatestyle='glass',
+                  template_style='glass',
                   view='L',
                   arrowaxis=None,
-                  subtitles='WHS')
+                  title='WHS',
+                  subtitles=None)
 
 plt.savefig('./figures/ratWHS.png', dpi=300)
 
 
 # Squirrel monkey VALiDATe29 
 netplotbrain.plot(template='VALiDATe29',
-                  templatestyle='glass',
+                  template_style='glass',
                   view='L',
                   arrowaxis=None,
-                  subtitles='VALiDATe29')
+                  title='VALiDATe29',
+                  subtitles=None)
 
 plt.savefig('./figures/squirrelmonkeyValidate29.png', dpi=300)
